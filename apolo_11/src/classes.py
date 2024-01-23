cea7f9e48718ae232df4b898c51165302f483196
Jose Mosquera <jojumofu@gmail.com>
2024-01-22 20:42:23 -0500

    feat: create config access from *.yaml file and create classes

---
+++ apolo_11/src/classes.py
@@ -0,0 +1,31 @@
+from .config import ConfigManager
+
+config_path = 'apolo_11/config/config.yaml'
+config = ConfigManager.read_yaml_config(config_path)
+
+class Mission:
+    def __init__(self):
+        self._name = config['mission']['names']
+        self._codes = config['mission']['codes']
+        
+    @property
+    def codes(self):
+        return self._codes
+      
+    @property
+    def name(self):
+        return self._name
+
+
+class Device:
+    def __init__(self):
+        self._type = config['devices']['types']
+        self._status = config['devices']['status']
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def status(self):
+        return self._status
