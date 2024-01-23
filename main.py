2823531f23b2da2996ebceed9c58a1cc942d72af
Jose Mosquera <jojumofu@gmail.com>
2024-01-23 06:12:44 -0500

    feat: create generator class
    For folders, filename, contentfile, complete file, and internal functions

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from apolo_11.src import classes
+from apolo_11.src import generator, config
+
+def main():
+    config_instance = config.ConfigManager()
+    
+    generator_instance = generator.Generator()
+    generator_instance.generate_principal_folders()
+    # Only for testing purposes test with hardcode file
+    generator_instance.generate_files(num_files_min=1, num_files_max=5)
+
 
 if __name__ == '__main__':
-    pass
+    main()
