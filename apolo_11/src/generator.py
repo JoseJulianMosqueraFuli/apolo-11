cd242b6035402e82476c36db68d51f38070cc0fc
Jose Mosquera <jojumofu@gmail.com>
2024-01-25 14:31:00 -0500

    feat: modify generator and  add cycle counter file to gitignore

--- apolo_11/src/generator.py
+++ apolo_11/src/generator.py
@@ -7,7 +7,6 @@ from typing import Union
 from .config import ConfigManager
 from .classes import Mission, Device
 
-# Nitpick: find a way to import this from .yaml
 config_path: str = 'apolo_11/config/config.yaml'
 config: dict = ConfigManager.read_yaml_config(config_path)
 
@@ -17,17 +16,16 @@ class Generator:
         self.device_instance: Device = Device()
         self.generate_files_call_count: int = 0
 
-    def generate_principal_folders(self, base_path='./apolo_11/results') -> None:
-        folders: list = ['devices', 'backups', 'reports']
-        for folder in folders:
-            folder_path = os.path.join(base_path, folder)
-            os.makedirs(folder_path, exist_ok=True)
+    def generate_device_folder(self, base_path='./apolo_11/results') -> None:
+        folder: str = 'devices'
+        folder_path = os.path.join(base_path, folder)
+        os.makedirs(folder_path, exist_ok=True)
 
-    def generate_filename(self, mission_name, file_number)->str:
+    def generate_filename(self, mission_name, file_number) -> str:
         mission_code: str = config['missions']['codes'].get(mission_name, 'UNKN')
         return f"APL{mission_code}-{file_number:04d}.log"
 
-    def generate_contentfile(self, file_number: int)-> str:
+    def generate_contentfile(self, file_number: int) -> str:
         mission_name: str = random.choice(self.mission_instance.name)
         device_type: str = random.choice(self.device_instance.type)
         device_status: str = random.choice(self.device_instance.status)
@@ -37,41 +35,43 @@ class Generator:
         if mission_name in config['missions']['codes']:
             hash_value: int = self.generate_hash(current_date, mission_name, device_type, device_status)
             content = f"Date: {current_date}\nMission: {mission_name}\n" \
-                f"Device Type: {device_type}\nDevice Status: {device_status}\n" \
-                f"Hash: {hash_value}"
+                      f"Device Type: {device_type}\nDevice Status: {device_status}\n" \
+                      f"Hash: {hash_value}"
         else:
             hash_value = 'unknown'
             device_status = 'unknown'
             device_type = 'unknown'
             unique_id = uuid.uuid4()
             content = f"Date: {current_date}\nMission: {mission_name}\n" \
-                f"Device Type: {device_type}\nDevice Status: {device_status}\n" \
-                f"Hash: {hash_value}\nID: {unique_id}"
-            
+                      f"Device Type: {device_type}\nDevice Status: {device_status}\n" \
+                      f"Hash: {hash_value}\nID: {unique_id}"
 
         filename = self.generate_filename(mission_name, file_number)
 
         return filename, content
 
     def generate_files(self, num_files_min: int, num_files_max: int):
-        self.load_cycle_number()
-        times_stamp: str = datetime.now().strftime('%Y%m%d%H%M%S')
-        output_directory: str = self.create_output_directory(times_stamp, self.generate_files_call_count)
+        try:
+            self.load_cycle_number()
+            times_stamp: str = datetime.now().strftime('%Y%m%d%H%M%S')
+            output_directory: str = self.create_output_directory(times_stamp, self.generate_files_call_count)
 
-        random_number: int = random.randint(num_files_min, num_files_max)
+            random_number: int = random.randint(num_files_min, num_files_max)
 
-        for file_number in range(1, random_number + 1):
-            filename, file_content = self.generate_contentfile(file_number)
-            file_path: str = os.path.join(output_directory, filename)
+            for file_number in range(1, random_number + 1):
+                filename, file_content = self.generate_contentfile(file_number)
+                file_path: str = os.path.join(output_directory, filename)
 
-            with open(file_path, 'w') as file:
-                file.write(file_content)
+                with open(file_path, 'w') as file:
+                    file.write(file_content)
 
-        self.save_cycle_number()
+            self.save_cycle_number()
+
+        except KeyboardInterrupt:
+            print("Files generation stopped by keyboard interrupt")
 
     def load_cycle_number(self):
         try:
-            # Nitpick: Isn't the best option to 
             with open(os.path.join(os.path.dirname(__file__), 'cycle_number.txt'), 'r') as file:
                 self.generate_files_call_count = int(file.read().strip())
         except FileNotFoundError:
