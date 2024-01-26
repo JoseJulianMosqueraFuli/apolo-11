4c4fadcf40dcdf721a8333da5510f80e4e5aa1da
Sara Palacio <sarapalacio2002@gmail.com>
2024-01-26 17:58:15 -0500

    doc strings

--- apolo_11/src/generator.py
+++ apolo_11/src/generator.py
@@ -19,21 +19,42 @@ config_path: str = 'apolo_11/config/config.yaml'
 config: dict = ConfigManager.read_yaml_config(config_path)
 
 class Generator:
+    """Generate files with mission and devise data
+    
+    Attributes:
+        mission_instance (Mission): instance of the Mission class
+        device_instance (Device): instance of the Device class
+        generate_files_call_count (int): number of times generate_files method is called
+    
+    """
     def __init__(self):
         self.mission_instance: Mission = Mission()
         self.device_instance: Device = Device()
         self.generate_files_call_count: int = 0
 
     def generate_device_folder(self, base_path='./apolo_11/results') -> None:
+        """Generate folder for storing device files      
+        
+        """
         folder: str = 'devices'
         folder_path = os.path.join(base_path, folder)
         os.makedirs(folder_path, exist_ok=True)
 
     def generate_filename(self, mission_name, file_number) -> str:
+        """Generate a filename for a log file
+
+        Returns:
+            str: generated file name
+        """
         mission_code: str = config['missions']['codes'].get(mission_name, 'UNKN')
         return f"APL{mission_code}-{file_number:04d}.log"
 
     def generate_contentfile(self, file_number: int) -> str:
+        """Generate content for a log file
+        Returns:
+            str: containing file name and file content        
+        """
+        
         mission_name: str = random.choice(self.mission_instance.name)
         device_type: str = random.choice(self.device_instance.type)
         device_status: str = random.choice(self.device_instance.status)
@@ -59,6 +80,12 @@ class Generator:
         return filename, content
 
     def generate_files(self, num_files_min: int, num_files_max: int):
+        """Generate log files with random data
+
+        Args:
+            num_files_min (int): Minimum number of files to generate
+            num_files_max (int): Maximum number of files to generate
+        """
         try:
             self.load_cycle_number()
             times_stamp: str = datetime.now().strftime('%Y%m%d%H%M%S')
@@ -81,6 +108,11 @@ class Generator:
             logger.info("Generación de archivos interrumpida por teclado.")
 
     def load_cycle_number(self):
+        """
+        Load generate_files_call_count from a file or
+        set to 0 if not found
+        """
+        
         try:
             with open(os.path.join(os.path.dirname(__file__), 'cycle_number.txt'), 'r') as file:
                 self.generate_files_call_count = int(file.read().strip())
@@ -89,12 +121,24 @@ class Generator:
             self.save_cycle_number()
 
     def save_cycle_number(self):
+        """
+        Increment and save the generate_files_call_count to a file
+        """
         self.generate_files_call_count += 1
 
         with open(os.path.join(os.path.dirname(__file__), 'cycle_number.txt'), 'w') as file:
             file.write(str(self.generate_files_call_count))
 
     def create_output_directory(self, times_stamp: str, generate_files_call_count: int) -> str:
+        """Create the output directory path based on timestamp and call count        
+
+        Args:
+            times_stamp (str): timestamp for the output directory
+            generate_files_call_count (int): call count for the output directory
+
+        Returns:
+            str: output directory path
+        """
         current_directory: str = os.path.dirname(os.path.abspath(__file__))
         output_directory: str = os.path.join(current_directory, f"./../results/devices/cycle-{generate_files_call_count}-{times_stamp}-noreport")
 
@@ -102,5 +146,8 @@ class Generator:
         return output_directory
 
     def generate_hash(self, *args: Union[str, int]) -> int:
+        """
+        Generate hash number
+        """
         data: str = ''.join(map(str, args))
         return hash(data)
